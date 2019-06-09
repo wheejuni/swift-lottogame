c0a9a6626db80d8cad3ab8546b8b611e0b85ade4
wheejuni <wheejuni@gmail.com>
2019-06-09 12:25:27 +0900

    Add lotto type enum

---
+++ swift-lotto/model/LottoType.swift
@@ -0,0 +1,14 @@
+//
+//  LottoType.swift
+//  swift-lotto
+//
+//  Created by wheejuni on 09/06/2019.
+//  Copyright © 2019 wheejuni. All rights reserved.
+//
+
+import Foundation
+
+enum LottoType {
+    case userLotto
+    case computerLotto
+}
